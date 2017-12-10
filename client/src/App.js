144fe4c8385460d30786a22d7f5f5d5187a24cf8
shk172 <sangkim2017@u.northwestern.edu>
2017-12-09 22:46:58 -0500

    Made Score Tables Reusable, Takes in New Scores
    Hardcoded cells to take in and record new scores for each frame. Each
    player gets their own score table with own states since the tables are now
    reusable components. Needs to handle Strikes and Spares.

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+class Game extends Component {
   constructor(props){
     super(props);
     this.state={
-      players: {},
-      score: {},
-      nameValue: "",
+      name: this.props.player.name,
+      scores: this.props.player.scores,
+      newScore: 0,
     }
   }
 
-  componentDidMount(){
-
-  }
-
-  _addPlayer(){
-    var players = this.state.players;
-    players[this.state.nameValue] = {games: []};
+  _addScore(){
+    var scores = this.state.scores;
+    scores.push(this.state.newScore);
     this.setState({
-      players,
-      nameValue: "",
-    });
+      scores,
+      newScore: 0,
+    })
   }
 
   _handleStrike(name){
@@ -32,70 +28,114 @@ class App extends Component {
     console.log(name);
   }
 
+  _scoreChange(event){
+    this.setState({
+      newScore: event.target.value,
+    });
+  }
+
+  render(){
+    return(
+      <div key={this.state.name+"Scores"}>
+        <table>
+          <caption>{this.state.name}</caption>
+          <tbody>
+            <tr>
+              <th>Game</th>
+              <td colSpan="2">1</td>
+              <td colSpan="2">2</td>
+              <td colSpan="2">3</td>
+              <td colSpan="2">4</td>
+              <td colSpan="2">5</td>
+              <td colSpan="2">6</td>
+              <td colSpan="2">7</td>
+              <td colSpan="2">8</td>
+              <td colSpan="2">9</td>
+              <td colSpan="2">10</td>
+            </tr>
+            <tr>
+              <th>Scores</th>
+              <td>{this.state.scores[0]}</td>
+              <td>{this.state.scores[1]}</td>
+              <td>{this.state.scores[2]}</td>
+              <td>{this.state.scores[3]}</td>
+              <td>{this.state.scores[4]}</td>
+              <td>{this.state.scores[5]}</td>
+              <td>{this.state.scores[6]}</td>
+              <td>{this.state.scores[7]}</td>
+              <td>{this.state.scores[8]}</td>
+              <td>{this.state.scores[9]}</td>
+              <td>{this.state.scores[10]}</td>
+              <td>{this.state.scores[11]}</td>
+              <td>{this.state.scores[12]}</td>
+              <td>{this.state.scores[13]}</td>
+              <td>{this.state.scores[14]}</td>
+              <td>{this.state.scores[15]}</td>
+              <td>{this.state.scores[16]}</td>
+              <td>{this.state.scores[17]}</td>
+              <td>{this.state.scores[18]}</td>
+              <td>{this.state.scores[19]}</td>
+            </tr>
+          </tbody>
+        </table>
+        <select onChange={this._scoreChange.bind(this)}>
+          <option>0</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+          <option>6</option>
+          <option>7</option>
+          <option>8</option>
+          <option>9</option>
+        </select>
+        <button onClick={this._addScore.bind(this)}>Add Score</button>
+        <button onClick={this._handleStrike.bind(this, this.state.name)}>Strike</button>
+        <button onClick={this._handleSpare.bind(this, this.state.name)}>Spare</button>
+      </div>
+    )
+  }
+}
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      players: {},
+      nameValue: "",
+    }
+  }
+
+  componentDidMount(){
+
+  }
+
+  _addPlayer(){
+    var players = this.state.players;
+    players[this.state.nameValue] = {
+      name: this.state.nameValue,
+      scores: []
+    };
+    this.setState({
+      players,
+      nameValue: "",
+    });
+  }
+
   _updateNameValue(event){
     this.setState({
       nameValue: event.target.value
     })
   }
 
-  _updateScore(){
 
-  }
 
   render() {
     var playerScores = Object.keys(this.state.players).map((playerName)=>{
       return(
-        <div key={{playerName}+"Scores"}>
-          <table>
-            <caption>{playerName}</caption>
-            <tbody>
-              <tr>
-                <th>Game</th>
-                <td>1</td>
-                <td>2</td>
-                <td>3</td>
-                <td>4</td>
-                <td>5</td>
-                <td>6</td>
-                <td>7</td>
-                <td>8</td>
-                <td>9</td>
-                <td>10</td>
-                <td>Add Score</td>
-              </tr>
-              <tr>
-                <th>Scores</th>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>
-                  <select onChange={this._scoreChange}>
-                    <option>0</option>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                    <option>6</option>
-                    <option>7</option>
-                    <option>8</option>
-                    <option>9</option>
-                  </select>
-                  <button onClick={this._updateScore.bind(this)}>Add Score</button>
-                  <button onClick={this._handleStrike.bind(this, playerName)}>Strike</button>
-                  <button onClick={this._handleSpare.bind(this, playerName)}>Spare</button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <Game key={playerName} player={this.state.players[playerName]}/>
       )
     })
 
